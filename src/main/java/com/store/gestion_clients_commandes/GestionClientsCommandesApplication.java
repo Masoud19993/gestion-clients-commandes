a40c2a7bb9ca3e261dab44b6dd5ac1b5fccc8207
Masoud19993 <masoudkaramidarmarani@gmail.com>
2026-05-29 18:44:08 +0200

    Initialiser le projet Spring Boot Maven

---
+++ src/main/java/com/store/gestion_clients_commandes/GestionClientsCommandesApplication.java
@@ -0,0 +1,13 @@
+package com.store.gestion_clients_commandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClientsCommandesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClientsCommandesApplication.class, args);
+	}
+
+}
