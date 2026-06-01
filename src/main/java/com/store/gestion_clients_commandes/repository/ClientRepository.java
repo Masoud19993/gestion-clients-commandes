595cb7567c0bab39b75c761ae2aa42f8e6823d06
Masoud19993 <masoudkaramidarmarani@gmail.com>
2026-06-01 11:41:07 +0200

    modifier ClientRepository

--- src/main/java/com/store/gestion_clients_commandes/repository/ClientRepository.java
+++ src/main/java/com/store/gestion_clients_commandes/repository/ClientRepository.java
@@ -1,9 +1,6 @@
 package com.store.gestion_clients_commandes.repository;
 
 import com.store.gestion_clients_commandes.model.Client;
-import com.store.gestion_clients_commandes.model.Commande;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
